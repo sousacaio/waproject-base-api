f981eb8e7273f526d8b25dd35839b093bfc4b8df
Shouganaii <frias.caio@yahoo.com.br>
2021-05-23 12:05:20 -0300

    feat: creation of the order paginator controller

--- src/modules/admin/controllers/order.ts
+++ src/modules/admin/controllers/order.ts
@@ -1,15 +1,26 @@
+import { Query } from '@nestjs/common';
+import { Get } from '@nestjs/common';
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Order } from 'modules/database/models/orders';
+import { OrderRepository } from '../repositories/order';
 import { OrderService } from '../services/order';
+import { ListValidator } from '../validators/order/list';
 import { SaveValidator } from '../validators/order/save';
 
 @ApiTags('Admin: Orders')
 @Controller('/order')
 export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
+  constructor(private readonly orderService: OrderService, private readonly orderRepository: OrderRepository) {}
 
   @Post('')
   public async login(@Body() model: SaveValidator) {
     return this.orderService.save(model);
+  }
+
+  @Get('')
+  @ApiResponse({ status: 200, type: [Order] })
+  public async list(@Query() model: ListValidator) {
+    return this.orderRepository.list(model);
   }
 }
